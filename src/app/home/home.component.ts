2607a354357f8af3373658c0a2ffcd4d15d8765c
urvashi.sapate <urvashi.sapate@lkart.in>
2021-08-13 10:24:55 +0530

    comments added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { PlacesService } from '../places.service';
+import { Component, OnInit } from "@angular/core";
+import { PlacesService } from "../places.service";
 import {
   FormArray,
   FormBuilder,
@@ -8,75 +8,66 @@ import {
   NgForm,
   Validators,
 } from "@angular/forms";
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
 
-import { debounceTime, tap, switchMap, finalize } from 'rxjs/operators';
-import { map, startWith } from 'rxjs/operators';
-import { stringify } from '@angular/compiler/src/util';
+
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
- buildingsData=[];
- buildingsForm:FormGroup;
-  selectedData=[];
-  selectedId:number;
-  stars = [1,2,3,4,5];
-  selectedValue:number;
- details=false;
- searchControl=new FormControl("",Validators.required)
+  buildingsData = [];
+  buildingsForm: FormGroup;
+  selectedData = [];
+  selectedId: number;
+  stars = [1, 2, 3, 4, 5];
+  selectedValue: number;
+  details = false;
+  searchControl = new FormControl("", Validators.required);
   constructor(
-    private placesService:PlacesService,
-    private fb:FormBuilder,
+    private placesService: PlacesService,
+    private fb: FormBuilder,
     private http: HttpClient
-  ) { }
+  ) {}
   filteredOptions: Observable<string[]>;
   ngOnInit() {
-   // searchControl=new FormControl("",Validators.required)
-   this.getAllBuildings();
-   this.buildingsForm=this.fb.group(
-    {
-      searchControl:new FormControl("",Validators.required)
+   
+    this.getAllBuildings();
+    this.buildingsForm = this.fb.group({
+      searchControl: new FormControl("", Validators.required),
     });
-  //this.stars=[{count:1},{count:2},{count:3},{count:4},{count:5}]
-  console.log(this.stars);
+   
+    console.log(this.stars);
   }
- 
-  getAllBuildings()
-  {
-    this.placesService.getPlaces().subscribe(
-      (res:any)=>
-      {
-        if(res)
-        {
-          this.buildingsData=res;
-          console.log(this.buildingsData);
-        }
-      })
-  }
-  onSelectBuilding(id)
-  {
-   this.selectedId=id;
-   this.selectedValue=0;
-  console.log(id);
-  
-  this.selectedData=this.buildingsData.filter((place)=>place.id==id);
-  console.log(this.selectedData);
-  }
-  countStar(star) {
-    this.selectedValue=star;
-   for(var i=0;i<this.buildingsData.length;i++)
-   {
-     if(this.buildingsData[i].id==this.selectedId)
-      {
-        this.buildingsData[i].ratings=star
+//Api call for get buildings
+  getAllBuildings() {
+    this.placesService.getPlaces().subscribe((res: any) => {
+      if (res) {
+        this.buildingsData = res;
+       
       }
-   }
-  
-    localStorage.setItem('ratings',star);
+    });
   }
- 
+  //On selction of Building
+  onSelectBuilding(id) {
+    this.selectedId = id;
+    this.selectedValue = 0;
+   
+     //filtering the data from array by selected Id
+    this.selectedData = this.buildingsData.filter((place) => place.id == id);
+    console.log(this.selectedData);
+  }
+//on click of ratings
+  countStar(star) {
+    this.selectedValue = star;
+    for (var i = 0; i < this.buildingsData.length; i++) {
+      if (this.buildingsData[i].id == this.selectedId) {
+        this.buildingsData[i].ratings = star;
+      }
+    }
+
+    
+  }
 }
